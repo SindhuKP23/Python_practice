ecbf0f879ffbd18437d555107548ec28bf65eb43
Sindhu KP <sindhukp@yahoo.in>
2025-11-17 20:55:11 +0530

    first commit

---
+++ LargestOfThree.py
@@ -0,0 +1,9 @@
+a= int(input("Enter first number: "))
+b= int(input("Enter second number: "))
+c= int(input("Enter third number: "))
+if a>b and a>c:
+    print(f"{a} is greater among {b} and {c}")
+elif b>a and b>c:
+    print(f"{b} is greater among {a} and {c}")
+else:
+    print(f"{c} is greater among {a} and {b}")
